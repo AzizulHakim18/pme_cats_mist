1b45399ab10354a603d83febe10d5bf0100221aa
AzizulHakim18 <hakimrony18@gamil.com>
2023-05-30 02:06:28 +0600

    navbar done

--- src/Sheared/Header/Header.js
+++ src/Sheared/Header/Header.js
@@ -1,9 +1,47 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import mist from './../../asset/img/mist.png'
 const Header = () => {
     return (
-        <div>
-            this is header seciton
+        <div className="navbar bg-base-100">
+            <div className="flex justify-start">
+
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link to='/'>HOME</Link></li>
+                        <li><Link to='/testrates'>TEST RATES</Link></li>
+                        <li><Link to='/consultancy'>CONSULTANCY</Link></li>
+                        <li><Link to='/experts'>EXPERTS</Link></li>
+                        <li><Link to='/about'>ABOUT</Link></li>
+                        <li><Link to='/contact'>CONTACT</Link></li>
+                        <li><Link to='/faq'>FAQ</Link></li>
+
+                    </ul>
+                </div>
+                <div className='flex  justify-start items-center'>
+                    <img src={mist} alt="" className='w-12 h-12 m-4' />
+                    <div >
+                        <h2 className='lg:text-2xl sm:text-xl font-bold'>CATS MIST (PME)</h2>
+                        <p className='text-sm font-semibold'>CENTRE FOR ADVISORY & TESTING SERVICES</p>
+                        <p className='text-sm font-semibold'>DEPT OF PETROLEUM & MINING ENGINEERING, MIST</p>
+                    </div>
+                </div>
+
+            </div>
+            <div className="navbar-end hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><Link to='/'>HOME</Link></li>
+                    <li><Link to='/testrates'>TEST RATES</Link></li>
+                    <li><Link to='/consultancy'>CONSULTANCY</Link></li>
+                    <li><Link to='/experts'>EXPERTS</Link></li>
+                    <li><Link to='/about'>ABOUT</Link></li>
+                    <li><Link to='/contact'>CONTACT</Link></li>
+                    <li><Link to='/faq'>FAQ</Link></li>
+                </ul>
+            </div>
         </div>
     );
 };
